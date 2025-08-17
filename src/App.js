a95067b58bddf93662de1e46ac129e570cc7f533
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-17 20:47:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
+import './App.css';
+
+import BestRest from './Components/BestRest';
+import Footer from './Components/Footer';
+import Navigate from './Components/Navigate';
+import OffersBanner from './Components/OffersBanner';
+import RestaurentChain from './Components/RestaurentChain';
+import RestaurentOnline from './Components/RestaurentOnline';
+
+function App() {
+  return (
+    <div>
+      {/* Navigation Bar */}
+      <Navigate />
+
+      {/* Promotional Offers Banner */}
+      <OffersBanner />
+
+      {/* Restaurant Chains Section */}
+      <RestaurentChain />
+
+      {/* Online Restaurants Section */}
+      <RestaurentOnline />
+
+      {/* Best Rated Restaurants Section */}
+      <BestRest />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
 
