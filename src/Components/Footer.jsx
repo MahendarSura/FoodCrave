909c6c120c38fa9d0d73bece2129552c18b47963
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-18 22:16:27 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -46,7 +46,6 @@ function Footer() {
                     <p>Mumbai</p>
                     <p>Pune </p>
                     <button style={{padding:"10px",background:"transparent",color:"#9e9e9e",border:"1px solid rgba(196, 198, 202, 0.1)",borderRadius:"20px"}}>589 cities <i className="ms-5 fa-solid fa-angle-down"></i></button>
-                </div>
             </div>
        </div>
 
