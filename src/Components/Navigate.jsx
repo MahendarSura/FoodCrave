122b09984707c82c6f80146e50fa0b23dc3112b4
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-20 13:45:06 +0530

    Update Navigate.jsx

--- src/Components/Navigate.jsx
+++ src/Components/Navigate.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import './Navigate.css'
+import logo from '../Photos/Logo.png' // ✅ Using the correct Foodcrave logo from src/Photos
 
 function Navigate() {
     return (
         <div>
             <Row className='w-100 pt-3 shadow pb-2'>
-                <Col >
-                    <div id='Nav-logo' style={{gap:"30px"}} className='d-flex justify-content-center align-items-center'>
-                        <img width={'55px'} src="https://www.theknowhowlib.com/wp-content/uploads/2020/05/Foodcrave-2.png" alt="lolll" />
-                        <p style={{fontSize:"13px"}} className='mt-3'><span className='fw-bold text-decoration-underline'>Mancherial</span>  Near IB Chowrastha...</p>
-                        <i style={{color:"#e78838"}} className="fa-solid fa-angle-down"></i>
-                        </div>
+                <Col>
+                    <div
+                        id='Nav-logo'
+                        style={{ gap: '30px' }}
+                        className='d-flex justify-content-center align-items-center'
+                    >
+                        {/* ✅ Local Foodcrave logo image */}
+                        <img width={'55px'} src={logo} alt="Foodcrave Logo" />
+                        <p style={{ fontSize: '13px' }} className='mt-3'>
+                            <span className='fw-bold text-decoration-underline'>Mancherial</span> Near IB Chowrastha...
+                        </p>
+                        <i
+                            style={{ color: '#e78838' }}
+                            className='fa-solid fa-angle-down'
+                        ></i>
+                    </div>
                 </Col>
                 <Col>
-                        <div id='Nav-icons' className='d-flex justify-content-between align-items-center w-75 mt-3'>
-                            <p><i className="fa-solid fa-magnifying-glass"> </i> Search</p>
-                            <p><i className="fa-solid fa-percent"></i> Offers <sup style={{color:"#fda502"}}>New</sup> </p>
-                            <p><i className="fa-solid fa-bowl-food"></i> Help</p>
-                            <p><i className="fa-regular fa-user"></i> Profile</p>
-                            <p><i className="fa-solid fa-cart-shopping"></i> Cart</p>
-                        </div>
+                    <div
+                        id='Nav-icons'
+                        className='d-flex justify-content-between align-items-center w-75 mt-3'
+                    >
+                        <p><i className="fa-solid fa-magnifying-glass"></i> Search</p>
+                        <p><i className="fa-solid fa-percent"></i> Offers <sup style={{ color: '#fda502' }}>New</sup></p>
+                        <p><i className="fa-solid fa-bowl-food"></i> Help</p>
+                        <p><i className="fa-regular fa-user"></i> Profile</p>
+                        <p><i className="fa-solid fa-cart-shopping"></i> Cart</p>
+                    </div>
                 </Col>
             </Row>
         </div>
@@ -28,4 +42,3 @@ function Navigate() {
 }
 
 export default Navigate
-
